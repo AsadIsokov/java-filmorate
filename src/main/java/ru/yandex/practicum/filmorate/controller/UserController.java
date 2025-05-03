cb9fff5fe283c4b86a670dc5bbe923d5087aeb29
Asad Isokov <isokov.asad@mail.ru>
2025-05-03 20:42:52 +0300

    После автотестов

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -17,13 +17,13 @@ public class UserController {
     private final Map<Long, User> users = new HashMap<>();
 
     @GetMapping
-    public Collection<User> allUsers(){
+    public Collection<User> allUsers() {
         log.info("Получен запрос на получение всех пользователей. Текущее количество: {}", users.size());
         return users.values();
     }
 
     @PostMapping
-    public User addUser(@RequestBody User user){
+    public User addUser(@RequestBody User user) {
         log.info("Получен запрос на добавление пользователя: {}", user);
         log.info("Проверка на валидность");
         if (user.getName() == null || user.getName().trim().isEmpty()) {
@@ -38,7 +38,7 @@ public class UserController {
     }
 
     @PutMapping
-    public User updateUser(@RequestBody User newUser){
+    public User updateUser(@RequestBody User newUser) {
         log.info("Получен запрос на обновление пользователя с ID: {}", newUser.getId());
         if (!users.containsKey(newUser.getId())) {
             log.error("Пользователь с ID {} не найден", newUser.getId());
@@ -69,17 +69,17 @@ public class UserController {
         return ++currentMaxId;
     }
 
-    private void validate(User user){
-        if(user.getEmail().isEmpty() || user.getEmail().isBlank()
-                || !user.getEmail().contains("@")){
+    private void validate(User user) {
+        if (user.getEmail().isEmpty() || user.getEmail().isBlank()
+                || !user.getEmail().contains("@")) {
             log.error("Ошибка валидации пользователя: неверный email {}", user.getEmail());
             throw new ValidationException("Электронная почта не может быть пустой и должна содержать символ @");
         }
-        if(user.getLogin().isEmpty() || user.getLogin().contains(" ")){
+        if (user.getLogin().isEmpty() || user.getLogin().contains(" ")) {
             log.error("Ошибка валидации пользователя: неверный login {}", user.getLogin());
             throw new ValidationException("Логин не может быть пустым и содержать пробелы");
         }
-        if(user.getBirthday().isAfter(LocalDate.now())){
+        if (user.getBirthday().isAfter(LocalDate.now())) {
             log.error("Ошибка валидации пользователя: дата рождения в будущем {}", user.getBirthday());
             throw new ValidationException("Дата рождения не может быть в будущем");
         }
